24f99ca8cc5c16805843eab200d3d0bf3bf2b5b7
leij11 <leij11@hotmail.com>
2020-07-18 07:34:27 -0400

    Style more

--- front_end/src/product/components/ProductDetails.js
+++ front_end/src/product/components/ProductDetails.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Card,Image, Rating } from 'semantic-ui-react';
+import { List } from 'semantic-ui-react'
 
 const ProductDetails = props => {
   return(
@@ -14,19 +15,26 @@ const ProductDetails = props => {
             <h3>{props.items.name}</h3>
           </li>
           <li>
-            <h3>Price: $<b>{props.items.price}</b></h3>
+            <h3>Price : $<b>{props.items.price}</b></h3>
           </li>
           <li>
-            <Rating icon='star' defaultRating={3} maxRating={5} disabled size='small'/>
+            <h5>Color: {props.items.color}</h5>
           </li>
           <li>
-            {props.items.description}
+            Brand : {props.items.brand}
+          </li>
+          <li>
+            Description: {props.items.description}
+          </li>
+          <li>
+            Product ID : {props.items._id}
           </li>
 
         </ul>
       </div>
       </div>
   )
+
 }
 
 export default ProductDetails;
